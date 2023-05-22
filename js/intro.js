317656c1731663fb6dbb15eb265e98c04fe32be1
Bhavya Shah <bhavyasha@google.com>
2023-05-21 17:02:17 -0700

    remove old junk

--- js/intro.js
+++
@@ -1,9 +0,0 @@
-// for window load
-	$(window).on('load',function() {
-		// Animate loader off screen
-		console.warn('load');
-    $(".loadingText").animate({
-      opacity:'0.5'
-    });
-		$(".icon").fadeOut("slow" );
-	});
